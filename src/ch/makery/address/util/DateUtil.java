d4355dce974feca7e1f02fbdfab59006dbcb72f2
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2018-04-15 16:23:30 +0200

    AdressApp Tutorial - Part 3 finished and it works
    HINT: don't use SceneBuilder inside IntelliJ -> terribly slow.
    Just start SceneBuilder as standalone-app -> works perfectly!
    I use this official (free) SceneBuilder: http://gluonhq.com/products/scene-builder/

--- src/ch/makery/address/util/DateUtil.java
+++ src/ch/makery/address/util/DateUtil.java
@@ -12,7 +12,7 @@ import java.time.format.DateTimeParseException;
 public class DateUtil {
 
     /** The date pattern that is used for conversion. Change as you wish. */
-    private static final String DATE_PATTERN = "DD.MM.YYYY";
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
 
     /** The date formatter. */
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_PATTERN);
@@ -55,7 +55,7 @@ public class DateUtil {
         return DateUtil.parse(dateString) != null;
     }
 
-
-
-
+    public static String getDatePattern() {
+        return DATE_PATTERN;
+    }
 }
