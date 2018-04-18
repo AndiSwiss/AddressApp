1a0289c665a95e9ddcfaaa63d7ba3ce12f99dae2
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2018-04-18 18:40:01 +0200

    AdressApp Tutorial - Part 5  - Problem solved thanks to IntelliJ (but Tutorial not yet finished):
    I put the cursor to the import-statement "import javax.xml.bind.annotation.XmlElement;"
    in PersonListWrapper.java which was not working. Then "alt"+"enter" -> there was the solution
    provided by IntelliJ:
    "Add '--add-modules=java.xml.bind' to module compiler options"
    
    -> worked perfectly. This option was written this in the file /.idea/compiler.xml (also in this commit)

--- src/ch/makery/address/model/PersonListWrapper.java
+++ src/ch/makery/address/model/PersonListWrapper.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+
 /**
  * Helper clas to wrap a list of persons. This is used for
  * saving the list of persons to XML.
